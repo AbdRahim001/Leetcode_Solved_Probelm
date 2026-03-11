4140e3bc1951c0cd2ce4f571b325b5faa5117da9
Abdur Rahim <captainabdurrahim@gmail.com>
2026-03-11 15:34:38 +0600

    Time: 0 ms (100.00%) | Memory: 9.6 MB (73.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t) {
-            return true;
+        if (s.size() != t.size()) {
+            return false;
         }
-        return false;
+        int count[26] = {0};
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        for (auto i : count) {
+            if (i != 0)
+                return false;
+        }
+        return true;
     }
 };
