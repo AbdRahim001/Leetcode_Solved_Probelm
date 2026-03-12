f263815a55bcb7613277132c406d34fc4ff8d791
Abdur Rahim <captainabdurrahim@gmail.com>
2026-03-12 19:31:40 +0600

    Time: 0 ms (100.00%) | Memory: 10.3 MB (24.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= 'a' && s[i] <= 'z') {
-                s1.push_back(s[i]);
-            } else if (s[i] >= 'A' && s[i] <= 'Z') {
-                s1.push_back(s[i] + 32);
-            } else if (s[i] >= '0' && s[i] <= '9') {
-                s1.push_back(s[i]);
-            }
+            string s1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            s1.push_back(s[i]);
         }
-        string s2 = s1;
-        reverse(s2.begin(), s2.end());
-        if (s1 == s2) {
-            return true;
-        } else {
+        else if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s1.push_back(s[i] + 32);
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            s1.push_back(s[i]);
+        }
+    }
+    for (int i = 0; i < floor(s1.size() / 2); i++)
+    {
+        if (s1[i] != s1[s1.size() - i-1])
+        {
             return false;
         }
+    }
+    return true;
     }
 };
