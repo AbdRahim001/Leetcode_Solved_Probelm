02f363351a7477afc8bcc80de053144bfd632795
Abdur Rahim <captainabdurrahim@gmail.com>
2026-03-12 21:01:24 +0600

    Time: 0 ms (100.00%) | Memory: 97.3 MB (86.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
+        int minPrice = prices[0];
         int maxProfit = 0;
         for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else {
-                maxProfit = max(maxProfit, prices[i] - minPrice);
-            }
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
         }
         return maxProfit;
     }
