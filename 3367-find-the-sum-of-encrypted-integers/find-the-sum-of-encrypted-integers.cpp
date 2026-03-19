ded4b01d5322e427b5a9cba1af141e63ed3e2433
Abdur Rahim <captainabdurrahim@gmail.com>
2026-03-19 07:02:04 +0600

    Time: 4 ms (77.78%) | Memory: 28.2 MB (19.95%) - LeetSync

--- 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.cpp
+++ 3367-find-the-sum-of-encrypted-integers/find-the-sum-of-encrypted-integers.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int encrypt(int x) {
-        if (x > 0 && x < 10) {
+        string s = to_string(x);
+        if (s.size() == 1)
             return x;
+
+        char maxDigit = *max_element(s.begin(), s.end());
+        int result = 0;
+        for (int i = 0; i < s.size(); i++) {
+            result = result * 10 + (maxDigit - '0');
         }
-        int temp, time = 0, mxDigit = 0;
-        while (x != 0) {
-            temp = x % 10;
-            mxDigit = max(mxDigit, temp);
-            x /= 10;
-            time++;
-        }
-        int Temp = 0;
-        for (int i = 0; i < time; i++) {
-            Temp = Temp * 10 + mxDigit;
-        }
-        return Temp;
+        return result;
     }
     int sumOfEncryptedInt(vector<int>& nums) {
+        int sum = 0;
         for (int i = 0; i < nums.size(); i++) {
-            nums[i] = encrypt(nums[i]);
+            sum += encrypt(nums[i]);
         }
-        return accumulate(nums.begin(), nums.end(), 0);
+        return sum;
     }
 };
