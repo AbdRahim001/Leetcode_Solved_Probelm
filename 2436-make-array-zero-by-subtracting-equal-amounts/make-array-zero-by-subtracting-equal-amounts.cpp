efda8a70d8fe0543c739ef857c028992481a50df
Abdur Rahim <captainabdurrahim@gmail.com>
2026-03-27 02:00:31 +0600

    Time: 0 ms (100.00%) | Memory: 11.1 MB (47.93%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+    set<int>st;
+    for(auto i:nums){
+        if(i!=0)
+            st.insert(i);
+    }
+    return st.size();
+    }
+};
