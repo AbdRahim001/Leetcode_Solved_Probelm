5d757574c1cbc8367394c968e45c4514f15b8b6a
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-16 21:13:31 +0600

    Time: 0 ms (100.00%) | Memory: 8.2 MB (10.88%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -5,7 +5,12 @@ public:
             return 0;
         if (n == 1)
             return 1;
-        int res = fib(n - 1) + fib(n - 2);
-        return res;
+        vector<int> dp(n);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
     }
 };
