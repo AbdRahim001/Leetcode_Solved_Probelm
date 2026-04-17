c327523418420d1f16ba8c9168de00e5eb519ec6
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-17 23:54:38 +0600

    Time: 0 ms (100.00%) | Memory: 15.1 MB (5.96%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> mp;
+        for (auto i : nums1) {
+            mp[i]++;
+        }
+        vector<int> ans;
+        for (auto i : nums2) {
+            if (mp[i] > 0) {
+                ans.push_back(i);
+                mp[i]--;
+            }
+        }
+        return ans;
+    }
+};
