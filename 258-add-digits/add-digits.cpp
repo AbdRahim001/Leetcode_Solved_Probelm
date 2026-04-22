92d5c3a0f4339587bff8427b5a098571b0b2e4b4
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-22 23:35:00 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (7.46%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
-    int sumDigit(int x) {
-        int temp = 0;
-        while (x != 0) {
-            temp += x % 10;
-            x /= 10;
-        }
-        return temp;
-    }
     int addDigits(int num) {
-        while (num > 9) {
-            num = sumDigit(num);
-        }
-        return num;
+        if(num==0)return 0;
+    return 1+(num-1)%9;
     }
 };
