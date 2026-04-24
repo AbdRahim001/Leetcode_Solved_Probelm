005b77b6d9e8bf6981c742418e8691e6bad202c7
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-24 15:04:27 +0600

    Time: 0 ms (100.00%) | Memory: 11.9 MB (17.69%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -5,12 +5,12 @@ public:
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0)
                 ans.push_back("FizzBuzz");
+            else if (i % 3 != 0 && i % 5 != 0)
+                ans.push_back(to_string(i));
             else if (i % 3 == 0)
                 ans.push_back("Fizz");
             else if (i % 5 == 0)
                 ans.push_back("Buzz");
-            else
-                ans.push_back(to_string(i));
         }
         return ans;
     }
