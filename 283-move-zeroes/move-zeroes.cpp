a531fd1635ffb90bdc5295d05ce1d74ca3b55d4b
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-28 18:53:27 +0600

    Time: 189 ms (5.01%) | Memory: 23.9 MB (18.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int len = nums.size();
-        int i = 0;
-        while (len--) {
-            if (nums[i] == 0) {
-                nums.erase(nums.begin() + i);
-                nums.push_back(0);
-            } else
-                i++;
+        bool swapped;
+        for (int i = 0; i < len - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (nums[j] == 0) {
+                    swap(nums[j], nums[j + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
         }
     }
 };
