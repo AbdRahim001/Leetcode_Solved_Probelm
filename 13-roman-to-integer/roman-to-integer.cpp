b867cbab5bb7a41cdf4be1f1ad50828f2a06e149
Abdur Rahim <captainabdurrahim@gmail.com>
2026-04-29 10:55:06 +0600

    Time: 10 ms (31.61%) | Memory: 13.2 MB (58.90%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,24 +5,8 @@ public:
                              {'C', 100}, {'D', 500}, {'M', 1000}};
         int val = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] == 'I' && s[i + 1] == 'V') {
-                val += 4;
-                i++;
-            } else if (s[i] == 'I' && s[i + 1] == 'X') {
-                val += 9;
-                i++;
-            } else if (s[i] == 'X' && s[i + 1] == 'L') {
-                val += 40;
-                i++;
-            } else if (s[i] == 'X' && s[i + 1] == 'C') {
-                val += 90;
-                i++;
-            } else if (s[i] == 'C' && s[i + 1] == 'D') {
-                val += 400;
-                i++;
-            } else if (s[i] == 'C' && s[i + 1] == 'M') {
-                val += 900;
-                i++;
+            if (i + 1 < s.length() && mp[s[i]] < mp[s[i + 1]]) {
+                val -= mp[s[i]];
             } else
                 val += mp[s[i]];
         }
