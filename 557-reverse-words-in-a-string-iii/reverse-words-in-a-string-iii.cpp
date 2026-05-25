1c5a969575a501838e87825e42dbd70dc06fe395
Abdur Rahim <captainabdurrahim@gmail.com>
2026-05-26 00:29:09 +0600

    Time: 0 ms (100.00%) | Memory: 12.2 MB (49.23%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<char> st;
-        string S;
+        int left = 0;
+        int right = 0;
         int len = s.size();
         for (int i = 0; i < len; i++) {
-            if (s[i] != ' ') {
-                st.push(s[i]);
-            }
+            if (s[i] != ' ')
+                right++;
             if (s[i] == ' ' || i + 1 == len) {
-                while (!st.empty()) {
-                    S.push_back(st.top());
-                    st.pop();
-                }
-                if (i + 1 != len)
-                    S.push_back(' ');
+                reverse(s.begin() + left, s.begin() + right);
+                left = i + 1;
+                right++;
             }
         }
-        return S;
+        return s;
     }
 };
