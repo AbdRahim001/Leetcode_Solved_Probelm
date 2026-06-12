9571ea7fac097902acdcc8baefffc502c7802679
Abdur Rahim <captainabdurrahim@gmail.com>
2026-06-12 12:37:30 +0600

    Time: 0 ms (100.00%) | Memory: 8.4 MB (71.83%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -1,30 +1,11 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        vector<string> vct;
         int len = s.size();
-        for (int i = 0; i < len - 2; i++) {
-            string z;
-            for (int j = i; j < i + 3; j++) {
-                z.push_back(s[j]);
-            }
-            vct.push_back(z);
-            z = "";
-        }
         int count = 0;
-        for (auto i : vct) {
-            map<char, int> mp;
-            for (auto j : i) {
-                mp[j]++;
-            }
-            bool flag = false;
-            for (auto k : mp) {
-                if (k.second > 1)
-                    flag = true;
-            }
-            if (!flag)
+        for (int i = 0; i < len - 2; i++) {
+            if (s[i] != s[i + 1] && s[i + 1] != s[i + 2] && s[i] != s[i + 2])
                 count++;
-            mp.clear();
         }
         return count;
     }
