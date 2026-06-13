403e05b7dbca4d4e87c4e7cef650beb1b76c609f
Abdur Rahim <captainabdurrahim@gmail.com>
2026-06-13 15:15:37 +0600

    Time: 3 ms (27.99%) | Memory: 43.5 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string s;
+        for (auto i : words) {
+            int temp = 0;
+            for (int j = 0; j < i.size(); j++)
+                temp += weights[i[j] - 97];
+            s.push_back('z' - temp % 26);
+        }
+        return s;
+    }
+};
