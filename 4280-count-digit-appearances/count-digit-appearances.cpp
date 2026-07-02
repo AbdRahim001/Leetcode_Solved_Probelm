9631574019305dbcda0715b1d160a439405bd30f
Abdur Rahim <captainabdurrahim@gmail.com>
2026-07-03 01:48:42 +0600

    Time: 0 ms (100.00%) | Memory: 37 MB (48.14%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.cpp
+++ 4280-count-digit-appearances/count-digit-appearances.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int digitSum(int n, int digit) {
-        int count = 0;
-        while (n != 0) {
-            int temp = n % 10;
-            if (temp == digit)
-                count++;
-            n /= 10;
-        }
-        return count;
-    }
     int countDigitOccurrences(vector<int>& nums, int digit) {
         int len = nums.size();
-        int sum = 0;
+        int count = 0;
         for (int i = 0; i < len; i++) {
-            sum += digitSum(nums[i], digit);
+            while (nums[i] != 0) {
+                int temp = nums[i] % 10;
+                if (nums[i] % 10 == digit)
+                    count++;
+                nums[i] /= 10;
+            }
         }
-        return sum;
+        return count;
     }
 };
