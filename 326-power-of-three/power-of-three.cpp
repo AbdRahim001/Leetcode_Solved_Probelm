7d5cc94413b558a35acef49ad9ee32ac820a3401
Abdur Rahim <captainabdurrahim@gmail.com>
2026-07-10 00:37:37 +0600

    Time: 2 ms (64.95%) | Memory: 9 MB (43.06%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n < 1)
+        if (n == 0)
             return false;
-        long long int a = 1162261467;
-        if (a % n == 0)
+        if (n == 1)
             return true;
-        else
-            return false;
+        if (n > 1)
+            return n % 3 == 0 && isPowerOfThree(n / 3);
+        return false;
     }
 };
