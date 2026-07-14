46e328bf4c8d95eb45cc67fbb6d568838feeda45
Abdur Rahim <captainabdurrahim@gmail.com>
2026-07-15 01:22:07 +0600

    Time: 7 ms (5.50%) | Memory: 25.9 MB (77.14%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -1,18 +1,25 @@
+//// Solved In-Place
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int len = nums.size();
-        vector<int> ans(len, 0);
-        int evenflag = 0, oddflag = 1;
         for (int i = 0; i < len; i++) {
-            if (nums[i] % 2 == 0) {
-                ans[evenflag] = nums[i];
-                evenflag += 2;
-            } else {
-                ans[oddflag] = nums[i];
-                oddflag += 2;
+            if (i % 2 == 0 && nums[i] % 2 != 0) {
+                for (int j = i + 1; j < len; j++) {
+                    if (j % 2 != 0 && nums[j] % 2 == 0) {
+                        swap(nums[i], nums[j]);
+                        break;
+                    }
+                }
+            } else if (i % 2 != 0 && nums[i] % 2 == 0) {
+                for (int j = i + 1; j < len; j++) {
+                    if (j % 2 == 0 && nums[j] % 2 != 0) {
+                        swap(nums[i], nums[j]);
+                        break;
+                    }
+                }
             }
         }
-        return ans;
+        return nums;
     }
 };
