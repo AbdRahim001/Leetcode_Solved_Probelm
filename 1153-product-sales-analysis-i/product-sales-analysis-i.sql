d5bb75e58c745d30c419bc08d15531035d0e9ed8
Abdur Rahim <captainabdurrahim@gmail.com>
2026-08-20 12:26:17 +0600

    Time: 1174 ms (71.02%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT P.product_name AS product_name,
-       S.year       AS year,
-       S.price      AS price
+       S.year         AS year,
+       S.price        AS price
 FROM   sales AS S
        JOIN product AS P
-         ON P.product_id = S.product_id; 
+       ON P.product_id = S.product_id; 
