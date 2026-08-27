ba2865c815e784e09c0f3da6565a4b47163f4a67
Abdur Rahim <captainabdurrahim@gmail.com>
2026-08-27 14:47:03 +0600

    Time: 195 ms (99.44%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,6 +1,6 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT score,
        Dense_rank()
          OVER (
-           ORDER BY score DESC) AS 'rank'
+           ORDER BY score DESC) AS "rank"
 FROM   scores; 
