2da7694876e2e3c6d37b19b2b7e52f5a17eb691b
Abdur Rahim <captainabdurrahim@gmail.com>
2026-09-08 14:19:36 +0600

    Time: 0 ms (100.00%) | Memory: 31.5 MB (93.56%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -7,20 +7,16 @@ public:
             freq1[i + 1000] = true;
         for (auto i : nums2)
             freq2[i + 1000] = true;
-        set<int> ans1;
-        set<int> ans2;
-        for (auto i : nums1)
-            if (freq2[i + 1000] == false)
-                ans1.insert(i);
-        for (auto i : nums2)
-            if (freq1[i + 1000] == false)
-                ans2.insert(i);
-        vector<int> result1;
-        vector<int> result2;
-        for (auto i : ans1)
-            result1.push_back(i);
-        for (auto i : ans2)
-            result2.push_back(i);
-        return {result1, result2};
+        vector<int> ans1;
+        vector<int> ans2;
+        for (int i = 0; i < 2002; i++) {
+            if (freq1[i] != freq2[i]) {
+                if (freq1[i])
+                    ans1.emplace_back(i - 1000);
+                else
+                    ans2.emplace_back(i - 1000);
+            }
+        }
+        return {ans1, ans2};
     }
 };
