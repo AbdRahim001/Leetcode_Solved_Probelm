47669091ef562489db08032389a139f210f21cd2
Abdur Rahim <captainabdurrahim@gmail.com>
2026-09-22 13:57:21 +0600

    Time: 3 ms (86.30%) | Memory: 12.5 MB (89.55%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -4,17 +4,4 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        a = 0
-        b = 0
-        for char in moves:
-            if char == "R":
-                a += 1
-            elif char == "L":
-                a -= 1
-            elif char == "U":
-                b += 1
-            elif char == "D":
-                b -= 1
-        if a == 0 and b == 0:
-            return True
-        return False
+        return moves.count("U") == moves.count("D") and moves.count("R") == moves.count("L")
